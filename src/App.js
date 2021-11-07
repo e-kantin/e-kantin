ded8c51b0b58b94d18afa8b2985b93d45a4e9410
Aras Nuri <33357572+NoorMoh@users.noreply.github.com>
2021-11-07 18:46:17 +0300

    Created project folder

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Router, BrowserRouter as Route, Switch } from 'react-router';
+
+function App() {
+  return (
+    <h2>Hello world</h2>
+  );
+}
+
+export default App;
