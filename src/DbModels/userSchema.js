230b73ae65053a3b1895b7ed0002fac0b8a11ae4
erenonur19 <eok@eskisehir.edu.tr>
2021-12-04 20:01:47 +0300

    post and user schemas

---
+++ src/DbModels/userSchema.js
@@ -0,0 +1,53 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const userSchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:3,
+        maxlength:15
+    },
+   surName:{
+    type:String,
+    required:true,
+    trim:true,
+    minlength:3,
+    maxlength:15,
+   },
+   userName:{
+    type:String,
+    required:true,
+    trim:true,
+    minlength:3,
+    maxlength:15,
+   },
+   //TC Kimlik Numarası
+   TCK:{
+    type:String,
+       required:true,
+       trim:true,
+       length:11, //**? */
+       unique:true,
+   },
+   Lectures:{
+       type:Array, //*//
+
+   },
+   email:{
+    type:String,
+    required:true,
+    minlength:3,
+    maxlength:30,
+    unique:true,
+},
+   password:{
+    type:String,
+    required:true,
+    minlength:3,
+    maxlength:100,
+},
+
+},{collection:'Users',timestamps:true});
+const User=mongoose.model('User',userSchema);
+module.exports=User;
